1c75945391e0c7ccb6a5e201e0a71371f7d21cb3
Sanchay Jain <sanchayjain@Sanchay-Jain.local>
2024-07-14 04:27:38 +0530

    env

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import uvicorn
 from fastapi import HTTPException, status, Depends, FastAPI
 from fastapi.security import APIKeyHeader
 import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 
 app = FastAPI()
 
@@ -12,6 +15,7 @@ auth_key = os.getenv('fast_api_demo_key')
 
 async def check_header(api_key: str = Depends(fast_api_demo_key)):
     if api_key != auth_key:
+        print(api_key,' is not a valid API key',auth_key)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key, Pass API key in header")
     return api_key
